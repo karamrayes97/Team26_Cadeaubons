a7fc2ef70c669ee25a2628ad7a70c1fa11e2b699
Dagmar Mergaert <260494126+DagmarStudent@users.noreply.github.com>
2026-03-20 19:29:52 +0100

    added StartUp project removed original presentation project and replaced with new one added domaincontroller to Domain

---
+++ Cadeaubons/Cadeaubons.StartUp/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Cadeaubons_Presentation;
+using System.Configuration;
+using System.Data;
+using System.DirectoryServices.ActiveDirectory;
+using System.Windows;
+using Cadeaubons_Domain;
+
+namespace Cadeaubons.StartUp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Domaincontroller dm = new();
+
+            CadeaubonsApplication application = new(dm);
+        }
+    }
+
+}
---
+++ Cadeaubons/Cadeaubons_Presentation2/CadeaubonsApplication.cs
@@ -0,0 +1,12 @@
+
+using System.DirectoryServices.ActiveDirectory;
+using Cadeaubons_Domain;
+
+namespace Cadeaubons_Presentation
+{
+    public class CadeaubonsApplication
+    {
+        public CadeaubonsApplication(Domaincontroller dm) { }
+    }
+
+}
