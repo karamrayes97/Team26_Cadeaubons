53873775f0fe234e8bb6baa154c7c8b419196e64
karamrayes97 <186206673+karamrayes97@users.noreply.github.com>
2026-05-14 19:25:41 +0200

    send email update

--- Cadeaubons/Cadeaubons_API/Services/Email/MailtrapEmailService.cs
+++ Cadeaubons/Cadeaubons_API/Services/Email/MailtrapEmailService.cs
@@ -6,35 +6,38 @@ using Microsoft.Extensions.Options;
 
 namespace Cadeaubons_API.Services.Email
 {
-    public class MailtrapEmailService : IEmailService
-    {
-        private readonly MailtrapOptions _options;
+	public class MailtrapEmailService : IEmailService
+	{
+		private readonly MailtrapOptions _options;
 
-        public MailtrapEmailService(IOptions<MailtrapOptions> options)
-        {
-            _options = options.Value;
-        }
+		public MailtrapEmailService(IOptions<MailtrapOptions> options)
+		{
+			_options = options.Value;
+		}
 
-        public async Task SendAsync(string to, string subject, string body)
-        {
-            var message = new MimeMessage();
+		public async Task SendAsync(string to, string subject, string body)
+		{
+			var message = new MimeMessage();
 
-            message.From.Add(new MailboxAddress(_options.FromName, _options.FromEmail));
-            message.To.Add(MailboxAddress.Parse(to));
-            message.Subject = subject;
+			message.From.Add(new MailboxAddress(_options.FromName, _options.FromEmail));
+			message.To.Add(MailboxAddress.Parse(to));
+			message.Subject = subject;
 
-            message.Body = new TextPart("html")
-            {
-                Text = body
-            };
+			message.Body = new TextPart("html")
+			{
+				Text = body
+			};
 
-            using var client = new SmtpClient();
+			using var client = new SmtpClient();
 
-            await client.ConnectAsync(_options.Host, _options.Port, MailKit.Security.SecureSocketOptions.StartTls);
-            await client.AuthenticateAsync(_options.Username, _options.Password);
+			client.CheckCertificateRevocation = false;
 
-            await client.SendAsync(message);
-            await client.DisconnectAsync(true);
-        }
-    }
+			await client.ConnectAsync(_options.Host, _options.Port, MailKit.Security.SecureSocketOptions.StartTls);
+			await client.AuthenticateAsync(_options.Username, _options.Password);
+
+
+			await client.SendAsync(message);
+			await client.DisconnectAsync(true);
+		}
+	}
 }
